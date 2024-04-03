a55317c8563196bcde2bed1a14abbe70969974ad
Sophia9156 <suhyun9156@gmail.com>
2024-04-03 19:41:17 +0900

    feat: added navigation bar

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,7 +1,13 @@
+import Navigation from "@/components/navigation";
 import { NextPage } from "next";
 
 const AboutUs: NextPage = () => {
-  return <h1>About Us!</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>About Us!</h1>
+    </div>
+  );
 };
 
 export default AboutUs;
