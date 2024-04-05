23f3b942a829c920d22bae8b115849ede7354a2d
Sophia9156 <suhyun9156@gmail.com>
2024-04-05 17:56:37 +0900

    feat: added data fetching feature

---
+++ app/movies/[id]/error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const ErrorPage = () => {
+  return <h1>Something Wrong...</h1>;
+};
+
+export default ErrorPage;
--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,12 +1,26 @@
+import MovieInfo from "@/components/movie-info";
+import MovieVideos from "@/components/movie-video";
 import { NextPage } from "next";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Movie Detail",
 };
 
-const MovieDetail: NextPage<{ params: { id: string } }> = ({ params }) => {
-  return <h1>Movie Detail {params.id}</h1>;
+const MovieDetailPage: NextPage<{ params: { id: string } }> = ({
+  params: { id },
+}) => {
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info...</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie video...</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 };
 
-export default MovieDetail;
+export default MovieDetailPage;
