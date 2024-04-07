ef48d82a77a60c57298204e7c3ebecfe030a0c5e
Sophia9156 <suhyun9156@gmail.com>
2024-04-07 14:16:46 +0900

    feat: added dynamic meta data

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,20 +1,39 @@
-import MovieInfo from "@/components/movie-info";
+import { API_URL } from "@/app/(home)/page";
 import MovieVideos from "@/components/movie-video";
 import { NextPage } from "next";
 import type { Metadata } from "next";
 import { Suspense } from "react";
 
-export const metadata: Metadata = {
-  title: "Movie Detail",
-};
+async function getMovie(id: string) {
+  try {
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-const MovieDetailPage: NextPage<{ params: { id: string } }> = ({
+interface IParams {
+  params: { id: string };
+}
+
+export async function generateMetadata({
   params: { id },
-}) => {
+}: IParams): Promise<Metadata> {
+  const movie = await getMovie(id);
+
+  return {
+    title: movie.title,
+  };
+}
+
+const MovieDetailPage: NextPage<IParams> = async ({ params: { id } }) => {
+  const movie = await getMovie(id);
+
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info...</h1>}>
-        <MovieInfo id={id} />
+        <h1>{movie.title}</h1>
       </Suspense>
       <Suspense fallback={<h1>Loading movie video...</h1>}>
         <MovieVideos id={id} />
